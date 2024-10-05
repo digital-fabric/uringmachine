807debb09ed32a283098db9815b58b1e92f5eec7
Sharon Rosner <sharon@noteflakes.com>
2024-10-05 20:36:14 +0200

    Implement #spin method

--- lib/uringmachine.rb
+++ lib/uringmachine.rb
@@ -3,3 +3,16 @@
 require_relative './um_ext'
 
 UM = UringMachine
+
+class UringMachine
+  def spin(value = nil, &block)
+    Fiber.new do |resume_value| 
+      block.(resume_value)
+    rescue Exception => e
+      raise RuntimeError, "Unhandled fiber exception: #{e.inspect}"
+    ensure
+      self.yield
+    end.tap { |f| schedule(f, value) }
+
+  end
+end
