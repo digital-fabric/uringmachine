a8907c0fe16915668f0a5e369a872de457d0bf32
Sharon Rosner <sharon@noteflakes.com>
2024-10-05 20:36:30 +0200

    Rewrite echo server

--- examples/echo_server.rb
+++ examples/echo_server.rb
@@ -1,52 +1,30 @@
-require_relative '../lib/iou'
+require_relative '../lib/uringmachine'
 require 'socket'
 
 socket = TCPServer.open('127.0.0.1', 1234)
 puts 'Listening on port 1234...'
 
-@ring = IOU::Ring.new
+$machine = UringMachine.new
+$bgid = $machine.setup_buffer_ring(4096, 1024)
 
-@ring.prep_accept(fd: socket.fileno, multishot: true) do |c|
-  setup_connection(c[:result]) if c[:result] > 0
+def handle_connection(fd)
+  $machine.read_each(fd, $bgid) do |buf|
+    $machine.write(fd, buf)
+  end
+  puts "Connection closed by client fd #{fd}"
+rescue Exception => e
+  puts "Got error #{e.inspect}, closing connection"
+  $machine.close(fd) rescue nil
 end
 
-def setup_connection(fd)
+$machine.spin do
+  loop do
+    $machine.sleep 5
+    puts "pending: #{$machine.pending_count}"
+  end
+end
+
+$machine.accept_each(socket.fileno) do |fd|
   puts "Connection accepted fd #{fd}"
-
-  buffer = +''
-  echo_prep_read(fd, buffer)
+  $machine.spin(fd) { handle_connection(_1) }
 end
-
-def echo_prep_read(fd, buffer)
-  @ring.prep_read(fd: fd, buffer: buffer, len: 4096, buffer_offset: -1) do |c|
-    if c[:result] > 0
-      echo_lines(fd, buffer)
-      echo_prep_read(fd, buffer)
-    elsif c[:result] == 0
-      puts "Connection closed by client on fd #{fd}"
-    else
-      puts "Got error #{c[:result]} on fd #{fd}, closing connection..."
-      @ring.prep_close(fd: fd) do |c|
-        puts "Connection closed on fd #{fd}, result #{c[:result]}"
-      end
-    end
-  end
-end
-
-def echo_lines(fd, buffer)
-  sep = $/
-  sep_size = sep.bytesize
-  
-  while true
-    idx = buffer.index(sep)
-    if idx
-      line = buffer.slice!(0, idx + sep_size)
-      @ring.prep_write(fd: fd, buffer: line)
-    else
-      break
-    end
-  end
-end
-
-trap('SIGINT') { exit! }
-@ring.process_completions_loop
