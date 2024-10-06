b80d8b68d4b34ef3922f5086e891468cdf995bca
Sharon Rosner <sharon@noteflakes.com>
2024-10-06 22:19:43 +0200

    Update http_server example

--- examples/http_server.rb
+++ examples/http_server.rb
@@ -1,56 +1,50 @@
-require_relative '../lib/iou'
-require 'socket'
+# frozen_string_literal: true
+
+require_relative '../lib/uringmachine'
 require 'http/parser'
 
-def log(msg)
-  # return
-  STDERR.puts msg
-end
+@machine = UM.new
+@bgid = @machine.setup_buffer_ring(4096, 1024)
 
-socket = TCPServer.open('127.0.0.1', 1234)
-log 'Listening on port 1234...'
-
-@ring = IOU::Ring.new
-@bg_id = @ring.setup_buffer_ring(count: 1024, size: 4096)
-
-@ring.prep_accept(fd: socket.fileno, multishot: true) do |c|
-  setup_connection(c[:result]) if c[:result] > 0
-end
-
-def setup_connection(fd)
-  log "Connection accepted fd #{fd}"
-
+def http_handle_connection(fd)
+  # puts "Accepting connection on fd #{fd}"
   parser = Http::Parser.new
-  parser.on_message_complete = -> {
-    http_send_response(fd, "Hello, world!\n") do
-      @ring.prep_close(fd: fd)
-    end
-  }
-
-  http_prep_read(fd, parser)
-end
-
-def http_prep_read(fd, parser)
-  buffer = +''
-  @ring.prep_read(fd: fd, buffer: buffer, len: 4096) do |c|
-    if c[:result] > 0
-      http_prep_read(fd, parser)
-      parser << buffer
-    elsif c[:result] == 0
-      log "Connection closed by client on fd #{fd}"
-    else
-      log "Got error #{c[:result]} on fd #{fd}, closing connection..."
-      @ring.prep_close(fd: fd) do |c|
-        log "Connection closed on fd #{fd}, result #{c[:result]}"
-      end
-    end
+  done = nil
+  parser.on_message_complete = -> do
+    http_send_response(fd, "Hello, world!\n")
+    done = true
   end
+
+  buf = +''
+  while !done && @machine.read(fd, buf, 8192) > 0
+    parser << buf
+  end
+  # @machine.read_each(fd, @bgid) do
+  #   parser << _1
+  #   break if done
+  # end
+  # puts "Connection closed on fd #{fd}"
+rescue => e
+  puts "Error while handling connection on fd #{fd}: #{e.inspect}"
+ensure
+  @machine.close(fd) rescue nil
 end
 
 def http_send_response(fd, body)
-  msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\nContent-Length: #{body.bytesize}\r\n\r\n#{body}"
-  @ring.prep_write(fd: fd, buffer: msg)
+  # msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\nContent-Length: #{body.bytesize}\r\n\r\n#{body}"
+  msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\nContent-Length: #{body.bytesize}\r\n\r\n#{body}"
+  @machine.write(fd, msg)
 end
 
 trap('SIGINT') { exit! }
-@ring.process_completions_loop
+
+server_fd = @machine.socket(UM::AF_INET, UM::SOCK_STREAM, 0, 0)
+@machine.bind(server_fd, '127.0.0.1', 1234)
+@machine.listen(server_fd, UM::SOMAXCONN)
+puts 'Listening on port 1234'
+
+at_exit { @machine.close(server_fd) rescue nil }
+
+@machine.accept_each(server_fd) do |fd|
+  @machine.spin(fd) { http_handle_connection _1 }
+end
