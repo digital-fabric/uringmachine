2bdb082591418c459be3d51f78b44adf13291fec
Sharon Rosner <sharon@noteflakes.com>
2026-02-06 22:30:19 +0100

    Prevent hang on joining accept thread

--- test/test_ssl.rb
+++ test/test_ssl.rb
@@ -19,14 +19,14 @@ class SSLTest < UMBaseTest
     s2 = OpenSSL::SSL::SSLSocket.new(sock2, OpenSSL::SSL::SSLContext.new)
     s2.sync_close = true
 
-    t = Thread.new { s1.accept }
+    t = Thread.new { s1.accept rescue nil }
 
     s2.connect
 
     assert_equal 6, s1.write('foobar')
     assert_equal 'foobar', s2.readpartial(6)
   ensure
-    t.join
+    t.join(0.1)
     sock1&.close rescue nil
     sock2&.close rescue nil
   end
@@ -42,7 +42,7 @@ class SSLTest < UMBaseTest
     @machine.ssl_set_bio(s2)
     assert_equal true, s2.instance_variable_get(:@__um_bio__)
 
-    t = Thread.new { s1.accept }
+    t = Thread.new { s1.accept rescue nil }
 
     assert_equal 0, @machine.metrics[:total_ops]
     s2.connect
@@ -51,7 +51,7 @@ class SSLTest < UMBaseTest
     assert_equal 6, s1.write('foobar')
     assert_equal 'foobar', s2.readpartial(6)
   ensure
-    t&.join
+    t&.join(0.1)
     sock1&.close rescue nil
     sock2&.close rescue nil
   end
@@ -67,7 +67,7 @@ class SSLTest < UMBaseTest
     @machine.ssl_set_bio(s2)
     assert_equal true, s2.instance_variable_get(:@__um_bio__)
 
-    t = Thread.new { s1.accept }
+    t = Thread.new { s1.accept rescue nil }
 
     assert_equal 0, @machine.metrics[:total_ops]
     s2.connect
@@ -78,7 +78,7 @@ class SSLTest < UMBaseTest
     assert_equal 6, @machine.ssl_read(s2, buf, 6)
     assert_equal 'foobar', buf
   ensure
-    t&.join
+    t&.join(0.1)
     sock1&.close rescue nil
     sock2&.close rescue nil
   end
