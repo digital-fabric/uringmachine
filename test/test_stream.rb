797d1f062cbdccc474e0113e28783935b0ec717a
Sharon Rosner <sharon@noteflakes.com>
2026-03-14 13:37:18 +0100

    Fix test

--- test/test_stream.rb
+++ test/test_stream.rb
@@ -123,7 +123,7 @@ class StreamTest < StreamBaseTest
 
     stream.clear
     pp machine.metrics
-    assert_in_range 32, machine.metrics[:buffers_allocated]
+    assert_equal 32, machine.metrics[:buffers_allocated]
     assert_equal 18, machine.metrics[:buffers_free]
     assert_equal 256, machine.metrics[:segments_free]
     assert_equal 65536 * 8, machine.metrics[:buffer_space_allocated]
